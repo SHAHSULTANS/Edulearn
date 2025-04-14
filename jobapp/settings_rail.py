7e71351b714bccf5db37c7c4b341797b7d492279
Shah Sultan <sultan91234567@gmail.com>
2025-04-14 16:16:50 +0600

    add postrsql in setting updated

--- jobapp/settings_rail.py
+++ jobapp/settings_rail.py
@@ -3,3 +3,16 @@ from jobapp.settings import *
 from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # or mysql
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int),  # optional cast
+    }
+}
