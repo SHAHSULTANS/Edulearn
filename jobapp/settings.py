aaf1a5df843d812fb4c9215e512b845a47bdcebf
Shah Sultan <sultan91234567@gmail.com>
2025-04-14 10:54:07 +0600

    dj-database-url python-dotenv

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -1,4 +1,6 @@
 import os
+
+print(os.getenv("DATABASE_URL"))
 from pathlib import Path
 import dj_database_url
 from dotenv import load_dotenv
@@ -63,13 +65,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'jobapp.wsgi.application'
 
 # PostgreSQL from Railway (DATABASE_URL is set in Railway Environment Variables)
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+import os
+import dj_database_url
+
+if os.getenv("RAILWAY_ENVIRONMENT") == "production":
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.getenv("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
