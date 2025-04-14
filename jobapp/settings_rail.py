19b17d88c89d96697b189432aca787e111e47910
Shah Sultan <sultan91234567@gmail.com>
2025-04-14 15:39:36 +0600

    add  jobapp/settings_rail.py manage_rail.py & install decuple

---
+++ jobapp/settings_rail.py
@@ -0,0 +1,5 @@
+from jobapp.settings import *
+
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
--- jobapp/wsgi.py
+++ jobapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobapp.settings_rail')
 
 application = get_wsgi_application()
