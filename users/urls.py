f2e1abe52537637fbd5f10c14d61e87a3d6d787f
Shah Sultan <sultan91234567@gmail.com>
2025-07-01 11:16:18 +0600

    Add password reset URL routes using Django's built-in auth views - Includes reset, reset done, confirm, and complete views - Uses custom templates under users/password/

--- users/urls.py
+++ users/urls.py
@@ -13,3 +13,18 @@ urlpatterns = [
    path('terms/', views.terms_of_service, name='terms_of_service'),
     
 ]
+
+
+
+
+
+
+from django.contrib.auth import views as auth_views
+urlpatterns += [
+    # Forgot Password URLs
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password/reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password/reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password/reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password/reset_complete.html'), name='password_reset_complete'),
+]
+
