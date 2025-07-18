7e840bd84c12309e01f5298c7484b9607363cc3d
Shah Sultan <sultan91234567@gmail.com>
2025-07-18 20:21:29 +0600

    Add Section model and related migration

--- courses/models.py
+++ courses/models.py
@@ -43,36 +43,15 @@ class Course(models.Model):
     
     
 
-# from django.db import models
-# from django.utils import timezone
-# from django.contrib.auth.models import User
+class Section(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='sections')
+    title = models.CharField(max_length=200)
+    order = models.PositiveIntegerField()
 
-# class Course(models.Model):
-#     title = models.CharField(max_length=200)
-#     description = models.TextField()
-#     duration = models.IntegerField(help_text="Duration in hours")
-#     thumbnail = models.ImageField(upload_to='course_thumbnail/', null=True, blank=True)
-#     created_at = models.DateTimeField( default=timezone.now)
+    class Meta:
+        ordering = ['order']
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return f"{self.course.title} - {self.title}"
+    
 
-# class Lesson(models.Model):
-#     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
-#     title = models.CharField(max_length=200)
-#     content = models.TextField()
-#     video_url = models.URLField(null=True, blank=True)
-#     completion_status = models.BooleanField(default=False)
-
-#     def __str__(self):
-#         return self.title
-
-# class Student(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="student")
-#     name = models.CharField(max_length=200)
-#     email = models.EmailField(unique=True)
-#     enrolled_courses = models.ManyToManyField(Course, related_name='students', blank=True)
-#     completed_lessons = models.ManyToManyField(Lesson, related_name='completed_by', blank=True)
-
-#     def __str__(self):
-#         return self.name
